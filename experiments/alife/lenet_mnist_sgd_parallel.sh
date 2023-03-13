247b32fd75f240c18d30fe4c7943a3e75f88921b
Alex Jackson <mail@alexjackson.uk>
2023-03-13 05:36:42 +0000

    Add parallel experiment

---
+++ experiments/alife/lenet_mnist_sgd_parallel.sh
@@ -0,0 +1,81 @@
+#! /bin/bash -l
+
+#SBATCH --job-name=lmsf
+#SBATCH --chdir=/users/k1502897/workspace/nets/
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu
+#SBATCH --mem=16G
+#SBATCH --signal=USR2
+#SBATCH --output=/scratch/users/%u/%A_%a.out
+#SBATCH --array=1-5
+
+set -euxo pipefail
+
+module load anaconda3/2021.05-gcc-10.3.0
+
+PYTHON_BIN="/scratch/users/k1502897/conda/nets/bin/python"
+
+# Define parameters for each array job task
+SEED=$((47 + $SLURM_ARRAY_TASK_ID))
+TRIAL=$SLURM_ARRAY_TASK_ID  # Used for filenames
+OUTPUT_DIR="/scratch/users/k1502897/alife_3"
+ARCH=lenet  # Architecture
+DATASET=mnist  # Target dataset
+OPTIMISER=sgd  # Optimiser
+LR=1e-3  # Learning rate
+MAX_ITER=50000  # Maximum (final) training iterations
+FITNESS=fwpass  # NeTS fitness function
+
+mkdir -p $OUTPUT_DIR
+
+# Run the experiment for the current task
+$PYTHON_BIN -m experiments.alife.experiment \
+    --trial $TRIAL \
+    --seed $SEED \
+    --out_dir $OUTPUT_DIR \
+    --arch $ARCH \
+    --dataset $DATASET \
+    --optimiser $OPTIMISER \
+    --lr $LR \
+    --max_iter $MAX_ITER \
+    --mr_disable 0 \
+    --mr_enable 0 \
+    --mr_random 0 \
+    --mr_noise 0.1 \
+    --fitness $FITNESS &
+
+# Run the experiment for the current task
+$PYTHON_BIN -m experiments.alife.experiment \
+    --trial $TRIAL \
+    --seed $SEED \
+    --out_dir $OUTPUT_DIR \
+    --arch $ARCH \
+    --dataset $DATASET \
+    --optimiser $OPTIMISER \
+    --lr $LR \
+    --max_iter $MAX_ITER \
+    --mr_disable 0 \
+    --mr_enable 0 \
+    --mr_random 0 \
+    --mr_noise 0.2 \
+    --fitness $FITNESS &
+
+# Run the experiment for the current task
+$PYTHON_BIN -m experiments.alife.experiment \
+    --trial $TRIAL \
+    --seed $SEED \
+    --out_dir $OUTPUT_DIR \
+    --arch $ARCH \
+    --dataset $DATASET \
+    --optimiser $OPTIMISER \
+    --lr $LR \
+    --max_iter $MAX_ITER \
+    --mr_disable 0 \
+    --mr_enable 0 \
+    --mr_random 0 \
+    --mr_noise 0.3 \
+    --fitness $FITNESS &
+
+wait
+
+echo "Job $SLURM_ARRAY_JOB_ID $SLURM_ARRAY_TASK_ID finished successfully"
